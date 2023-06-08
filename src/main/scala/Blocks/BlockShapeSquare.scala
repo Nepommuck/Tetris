3a66965086ecca806936d3ed6c9bb8cf7e429b28
Paweł Konopka <pawel.konopka5@gmail.com>
2023-06-08 16:23:13 +0200

    No square rotation

--- src/main/scala/Blocks/BlockShapeSquare.scala
+++ src/main/scala/Blocks/BlockShapeSquare.scala
@@ -1,11 +1,12 @@
 package Blocks
 
-import Utils.Vec2d
+import Utils.{RotateDirection, Vec2d}
 import scalafx.scene.paint.Color
 
 class BlockShapeSquare(position: Vec2d) extends PlayingBlock(
     color = Color.Yellow, position, contentAsList = List(
         (1, 0), (0, 1), (0, 0), (1, 1)
     )) {
-    
+
+    override def contentRotated(direction: RotateDirection): List[Vec2d] = content
 }
